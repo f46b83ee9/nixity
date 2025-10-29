36f29ca2cdd618ba37cce3bf6d97d7112cf73362
f46b83ee9 <me@vfd.ovh>
2025-10-29 12:42:13 +0100

    chore: update pocket-id

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,13 +1,13 @@
 self: super: {
   pocket-id = super.pocket-id.overrideAttrs (
     final: old: {
-      version = "1.14.1";
+      version = "1.14.2";
 
       src = super.fetchFromGitHub {
         owner = "pocket-id";
         repo = "pocket-id";
         tag = "v${final.version}";
-        hash = "sha256-PnZKlJ6smwpgijqQM5xTcopyGJfO2pebiEJhEIdAOFk=";
+        hash = "sha256-0x80oykzMTEsuGUBtXXdKgWJld8mrDPUssYpAznqKnc=";
       };
 
       vendorHash = "sha256-CmhPURPNwcpmD9shLrQPVKFGBirEMjq0Z4lmgMCpxS8=";
