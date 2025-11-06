5beef23a4573584a1a693df41663f5fe023d994a
f46b83ee9 <me@vfd.ovh>
2025-11-06 18:20:38 +0100

    feat: opkssh + yubikey ssh

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 {
-  services.openssh.enable = true;
-
   services.openssh = {
+    enable = true;
+
     allowSFTP = false;
 
     settings = {
@@ -22,5 +22,26 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMT48CcI5L0mXR03igIaNFo+j0WnEAqvQlG+eeZqr9jP"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOUeVZ5v2j2QOygXhsq+dueavCKDco1fnSS9hYf2/+BxAAAACHNzaDptYWlu"
   ];
+
+  services.opkssh = {
+    enable = true;
+
+    providers = {
+      pocket-id = {
+        issuer = "https://key.vfd.ovh";
+        clientId = "fd8c75a3-664c-46d8-b3d7-bf20b81cd817";
+        lifetime = "oidc";
+      };
+    };
+
+    authorizations = [
+      {
+        user = "root";
+        principal = "oidc:groups:ssh_administrators";
+        inherit (config.services.opkssh.providers.pocket-id) issuer;
+      }
+    ];
+  };
 }
